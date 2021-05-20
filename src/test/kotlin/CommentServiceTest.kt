6933a9eef199e8c8ab3090783f8f3e7531f62fa8
AvdeevAleksey <avdeev.hexer@yandex.ru>
2021-05-20 14:29:00 +0300

    Creating tests in progress.

---
+++ src/test/kotlin/CommentServiceTest.kt
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CommentServiceTest {
+
+    @Test
+    fun getComments() {
+    }
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun delete() {
+    }
+
+    @Test
+    fun edit() {
+    }
+
+    @Test
+    fun get() {
+    }
+
+    @Test
+    fun recovery() {
+    }
+}
---
+++ src/test/kotlin/NoteServiceTest.kt
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class NoteServiceTest {
+
+    @Test
+    fun getNotes() {
+    }
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun delete() {
+    }
+
+    @Test
+    fun edit() {
+    }
+
+    @Test
+    fun get() {
+    }
+
+    @Test
+    fun recovery() {
+    }
+
+    @Test
+    fun getById() {
+    }
+}
